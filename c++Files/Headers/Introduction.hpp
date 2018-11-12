0c71881770810cd79ca303e3e9c691c6e3960250
marvinmouroum <mouroum.m@gmail.com>
2018-11-12 16:21:51 +0100

    intro page

---
+++ c++Files/Headers/Introduction.hpp
@@ -0,0 +1,28 @@
+//
+//  Introduction.hpp
+//  LAR_1_0
+//
+//  Created by Air Marvin on 12.11.18.
+//  Copyright © 2018 Air Marvin. All rights reserved.
+//
+
+#ifndef Introduction_hpp
+#define Introduction_hpp
+
+/*! \mainpage Laboratory of Applied Robotics Documentation
+ * Authors:
+ * Riccardo
+ * Marvin
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
+#endif /* Introduction_hpp */
