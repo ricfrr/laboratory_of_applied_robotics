d14fa1ab98cfa55e0226ded375db3e4cda0e75b3
ricfrr <riccardo.franceschini@live.it>
2018-11-19 14:25:22 +0100

    refactor of path planning classesù

--- dubins/main.cpp
+++ dubins/main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include "Headers/Position.hpp"
-#include "Headers/Car.hpp"
-#include "Headers/DubinPath.hpp"
+#include "Headers/PathCoordinates.hpp"
+#include "Headers/Path.hpp"
 using namespace std;
 
-const double PI = 3.14159265358979323846;
 
-Car initialize();
+PathCoordinates initialize();
 
 int main(){
-    Car car = initialize();
-    DubinPath dubinPath = DubinPath(car);
-    dubinPath.dubinShortestPath();
+
+    PathCoordinates pathCoordinates = initialize();
+    Path path = Path(pathCoordinates.getInitialPosition(),pathCoordinates.getFinalPosition(),pathCoordinates.getMaxCurvature());
+
     return 0;
 }
 
-Car initialize(){
+PathCoordinates initialize(){
     // initial position
     int in_x = 0;
     int in_y = 1;
-    double in_orient = -1.0/3*PI;
-    double Kmax = 1.0;
+    double in_orient = -1.0/3*M_PI;
+    double Kmax = 1.0; //max curvature 1/kmax give us the radius
     cv::Point2d in_coordinates = Point2d(in_x,in_y);
     Position in_position = Position(in_coordinates,in_orient);
     // final position
     int f_x = 4;
     int f_y = 3;
-    double f_orient = PI/3.0;
+    double f_orient = M_PI/3.0;
     cv::Point2d f_coordinates = Point2d(f_x,f_y);
     Position f_position = Position(f_coordinates,f_orient);
 
 
-    Car car = Car(in_position,f_position,Kmax);
+    PathCoordinates car = PathCoordinates(in_position,f_position,Kmax);
     return car;
 }
