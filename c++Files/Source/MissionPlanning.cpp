ec10af5c6fe6e0e9a3fd498a46a3417c7e592695
marvinmouroum <mouroum.m@gmail.com>
2018-12-17 12:23:28 +0100

    some kind of second mission done

--- c++Files/Source/MissionPlanning.cpp
+++ c++Files/Source/MissionPlanning.cpp
@@ -11,11 +11,15 @@
 struct {
     bool operator()(People* a, People* b) const
     {
-        bool r = *a < *b;
-        std::cout << "result comp = " << r << std::endl;
         return *a < *b;
     }
 } customLess;
+
+
+double dinstance (cv::Point &p, const cv::Point &o) {
+    cv::Point point = p-o;
+    return std::sqrt(std::pow(point.x,2) + std::pow(point.y,2));
+}
 
 MissionPlanning::MissionPlanning(){
     this->map_p = new Map;
@@ -72,9 +76,70 @@ void MissionPlanning::plan_mission_one(){
     v.visualize();
     v.simulate();
     
+}
+
+
+void MissionPlanning::plan_mission_two(){
     
+    std::cout << "planning mission two" << std::endl;
+    
+    Digit_Recognition::PeopleStorage peops = map_p->getPeople();
+    std::vector<People> people_v = peops.circles;
+    std::vector<People*> people_s;
+    
+    cv::Point2d start(60,60);
+    
+    for(std::vector<People>::iterator it=people_v.begin();it != people_v.end(); it++)
+        people_s.push_back(&*it);
+    
+    std::sort(people_s.begin(), people_s.end(), [start](People* a, People* b) {
         
+        return dinstance(a->center, start) < dinstance(b->center,start);
+    });
+    
+    if(people_s.empty())
+    {
+        std::cout << "no people detected.. cannot plan mission" << std::endl;
+        return;
+    }
+    
+    double or1 = 0*M_PI;
+    double or2 = 1.5*M_PI;
+    std::pair<cv::Point,double> exit = map_p->getExitPoint().getEntryPoint();
+    Path2D::Position finalPos =  Path2D::Position(exit.first,exit.second);finalPos.orientation_locked = true;
+    
+    Path2D::Path* path = new Path2D::Path(Path2D::Position(cv::Point2d(60,60),or1),
+                                          finalPos,0.05,map_p);
+    
+    if(path->lines.empty()){
         
+        path = new Path2D::Path(Path2D::Position(cv::Point2d(60,60),or1),
+                                              Path2D::Position(people_s[0]->center,or2),0.05,map_p);
+        
+        for(int i = 1; i<people_s.size();i++){
+            
+            Path2D::Path* b_path = new Path2D::Path(path->end_point,
+                                                    finalPos,0.05,map_p);
+            
+            if(!b_path->lines.empty()){
+                path = new Path2D::Path(*path,*b_path);
+                break;
+            }
+            
+            Path2D::Path* p_path = new Path2D::Path(path->end_point,
+                                                    Path2D::Position(people_s[i]->center,or2),0.05,map_p);
+            
+            path = new Path2D::Path(*path,*p_path);
+        }
+        
+        Path2D::Path* p_path = new Path2D::Path(path->end_point,
+                                                finalPos,0.05,map_p);
+        path = new Path2D::Path(*path,*p_path);
+    }
+    
+    Visualizer v(*map_p,path);
+    v.visualize();
+    v.simulate();
 }
 
 
